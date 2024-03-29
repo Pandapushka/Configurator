0f11aad2595d352a05a70402af02b7552fa17fb2
Никита Андреев <andreev.nik.d@yandex.ru>
2024-03-29 17:03:00 +0300

    .

---
+++ Configurator/DataService/DeviseDataService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Configurator.DataService
+{
+    public class DeviseDataService
+    {
+        private readonly string _filePath;
+        private readonly string folderName = "TaskTurner";
+        private readonly string fileName = "devise.Json";
+
+        public DeviseDataService()
+        {
+           //Получаем полный путь к папке с данными
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string appFolder = Path.Combine(appDataPath, folderName);
+            
+        }
+    }
+}
