6ef2dffe869999c97610c188b8d0075f01166aa1
Никита Андреев <andreev.nik.d@yandex.ru>
2024-03-29 15:57:22 +0300

    переделал часть проекта по патерну MVVM

---
+++ Configurator/View/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Configurator.Model;
+using Configurator.Model.Entities;
+using Configurator.Pages;
+using System.Windows;
+
+namespace Configurator
+{
+
+    public partial class MainWindow : Window
+    {
+        List<Device> devices;
+        public MainWindow()
+        {
+            DeviseStorage storage = new DeviseStorage();
+            devices = storage.GetDevices();
+            InitializeComponent();
+            treeView1.ItemsSource = devices;
+        }       
+        private void ButtonClouse(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void OpenPage(object sender, RoutedEventArgs e)
+        {
+            FramePage.Content = new InfoPage();
+        }
+
+    }
+}
---
+++ Configurator/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Configurator.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public RelayCommand IOpenNewWindow => new RelayCommand(OpenAddNewDeviceWindow);
+        public RelayCommand ICheckDevice => new RelayCommand(CheckDevices);
+        public RelayCommand IDeleteDevices => new RelayCommand(DeleteDevices);
+        public RelayCommand ISave => new RelayCommand(Save);
+
+        private void OpenAddNewDeviceWindow()
+        {
+            AddDevicesWindow addDevicesWindow = new AddDevicesWindow();
+            addDevicesWindow.ShowDialog();
+        }
+        private void DeleteDevices()
+        {
+            MessageBox.Show("Удалить");
+        }
+        private void CheckDevices()
+        {
+            MessageBox.Show("Проверить");
+        }
+
+        private void Save()
+        {
+            MessageBox.Show("Сохранить");
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
